3a90d673c7f7003a2566b38235dc341b9a168df1
cristianpela <cristypela05@gmail.com>
2019-08-10 11:20:03 +0300

    screen size is the same for any orientation

--- app/src/main/java/com/crskdev/plashpuzzle/SystemAbstractions.kt
+++ app/src/main/java/com/crskdev/plashpuzzle/SystemAbstractions.kt
@@ -1,6 +1,7 @@
 package com.crskdev.plashpuzzle
 
 import android.content.Context
+import android.content.res.Configuration
 
 /**
  * Created by Cristian Pela on 05.08.2019.
@@ -17,6 +18,15 @@ class SystemAbstractionsImpl(private val context: Context): SystemAbstractions{
 
     override fun dp(int: Int): Int = int.dp(context.resources)
 
-    override val screenSize: Pair<Int, Int> = context.screenSize
+    override val screenSize: Pair<Int, Int>
+        get() {
+            val orientation = context.resources.configuration.orientation
+            val screeSize = context.screenSize
+            return if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                screeSize.second to screeSize.first // swap width and height
+            } else {
+                screeSize
+            }
+        }
 
 }
